4aa09a26a2ec2d4132f265dd72490a4d3471c5e5
Valdrin <valdrin@slicelife.com>
2021-12-09 17:00:42 +0100

    Refactor List - Add Pagination

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,25 +2,50 @@ import { Component } from "react";
 import MuiList from "@material-ui/core/List";
 import ListItem from "./ListItem";
 import ListSubheader from "@material-ui/core/ListSubheader";
+import { Todo } from "../types";
+import { Pagination } from "@mui/material";
 
-class List extends Component<{ items: any[] }> {
+type Props = {
+  items: Todo[];
+};
+class List extends Component<Props, { currentPage: number }> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      currentPage: 1,
+    };
+  }
+
   render() {
     const { items } = this.props;
-    let itemList: JSX.Element[] = []
-    items.forEach(item => {
-      itemList.push(<ListItem id={item.id} name={item.id} />)
-    })
+
+    const totalNumberOfPages = Math.ceil(items.length / 3);
+    const currentItems = items.slice(
+      (this.state.currentPage - 1) * 3,
+      this.state.currentPage * 3
+    );
+
+    let itemList: JSX.Element[] = [];
+    currentItems.forEach(item => {
+      itemList.push(<ListItem key={item.id} {...item} />);
+    });
 
     return (
-      <MuiList
-        subheader={
-          <ListSubheader component="div" id="nested-list-subheader">
-            List
-          </ListSubheader>
-        }
-      >
-        {itemList}
-      </MuiList>
+      <>
+        <MuiList
+          subheader={
+            <ListSubheader component="div" id="nested-list-subheader">
+              Todo List
+            </ListSubheader>
+          }
+        >
+          {itemList}
+        </MuiList>
+        <Pagination
+          count={totalNumberOfPages}
+          onChange={(_e, page) => this.setState({ currentPage: page })}
+        ></Pagination>
+      </>
     );
   }
 }
