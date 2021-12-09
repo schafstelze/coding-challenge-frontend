8e21eb375608f405dd5dff2f4afe41235f997bda
Valdrin <valdrin@slicelife.com>
2021-12-09 17:01:12 +0100

    Refactor ListItem

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,19 +1,35 @@
-import { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import MuiListItem from "@material-ui/core/ListItem";
-interface ListItemProps {
-  id: string;
-  name: string;
-}
+import { Todo } from "../types";
+import { makeStyles, Theme } from "@material-ui/core";
+const ListItem: React.FC<Todo> = ({ description, id, name }) => {
+  const useStyles = makeStyles((theme: Theme) => ({
+    wrapper: {
+      display: "flex",
+      flexDirection: "column",
+      fontFamily: "Roboto, sans-serif",
+    },
+    name: {
+      margin: "10px 0",
+    },
+    description: {
+      color: "#aaa",
+      margin: 0,
+    },
+  }));
+  const styles = useStyles();
 
-class ListItem extends Component<ListItemProps> {
-  render() {
-    return (
-      <Link to={`/details/${this.props.id}`}>
-        <MuiListItem button>some item {this.props.id}</MuiListItem>
-      </Link>
-    );
-  }
-}
+  return (
+    <Link to={`/details/${id}`}>
+      <MuiListItem button divider>
+        <div className={styles.wrapper}>
+          <p className={styles.name}>{name}</p>
+          <p className={styles.description}>{description}</p>
+        </div>
+      </MuiListItem>
+    </Link>
+  );
+};
 
 export default ListItem;
